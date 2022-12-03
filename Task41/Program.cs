06dce2658e37a767d9d3030f8f9cb3996150c930
HappyShark <shark-tex47@yandex.ru>
2022-12-04 02:56:59 +0300

    first commit

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+using static System.Console;
+Clear();
+
+Write("Введите количество чисел: ");
+int NumberOfNumbers = int.Parse(ReadLine()!);
+int count = 0;
+for (int i = 0; i < NumberOfNumbers; i++) 
+{
+    Write($"Введите число {i + 1}: ");
+    int TheNumberEntered = int.Parse(ReadLine()!);
+    if (TheNumberEntered > 0)
+        count += 1;
+}
+Console.WriteLine($"Чисел больше 0: {count}");
---
+++ Task43/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 43: Напишите программу, 
+//которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+using static System.Console;
+Clear();
+
+Write("Введите значение b1: ");
+double b1 = double.Parse(ReadLine()!);
+Write("Введите значение k1: ");
+double k1 = double.Parse(ReadLine()!);
+Write("Введите значение b2: ");
+double b2 = double.Parse(ReadLine()!);
+Write("Введите значение k2: ");
+double k2 = double.Parse(ReadLine()!);
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+if (k1 != k2)
+    Console.WriteLine($"Пересечение прямых по координатам: ({x}; {y})");
+else
+    Console.WriteLine("Пересечение невозможно");
